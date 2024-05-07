b54995a89ab4ac7e46b8c77e744f578e30c1b7ac
sohunz <sohunily@gmail.com>
2024-05-07 21:57:18 +0700

    UI done

---
+++ src/components/Home.tsx
@@ -0,0 +1,52 @@
+import { FiSearch } from "react-icons/fi";
+import cloud from "../assets/cloud.png";
+import wind from "../assets/wind.png";
+import humidity from "../assets/humidity.png";
+
+const Home = () => {
+    return (
+        <div className="w-full h-screen bg-violet-900 flex flex-col items-center justify-center">
+            <div className="max-w-[480px] min-w-[480px] pt-10 rounded-xl p-10  shadow bg-violet-800">
+                <div className="w-full flex gap-3 justify-center items-center">
+                    <input
+                        className="appearance-none border-2 border-gray-200 rounded-full py-4 px-5 text-gray-800 leading-tight focus:outline-none bg-gray-200 focus:border-purple-500 focus:bg-white w-full"
+                        type="text"
+                        placeholder="Search"
+                    />
+                    <div className="border p-4 rounded-full bg-gray-200 cursor-pointer">
+                        <FiSearch size={23} />
+                    </div>
+                </div>
+                <div className="flex flex-col justify-center items-center my-5">
+                    <div className="max-w-[190px] mb-2">
+                        <img src={cloud} alt="" />
+                    </div>
+                    <p className="text-white text-7xl font-bold mb-3">24 °C</p>
+                    <p className="text-white text-4xl font-bold">London</p>
+                </div>
+                <div className="flex items-center justify-between mt-10">
+                    <div className="flex gap-2">
+                        <div className="max-w-[30px] mt-2">
+                            <img src={humidity} alt="" />
+                        </div>
+                        <div className="text-gray-300">
+                            <p className="text-2xl font-semibold">87%</p>
+                            <p className="text-lg font-semibold">Humidity</p>
+                        </div>
+                    </div>
+                    <div className="flex gap-2">
+                        <div className="max-w-[35px] mt-[5px]">
+                            <img src={wind} alt="" />
+                        </div>
+                        <div className="text-gray-300">
+                            <p className="text-2xl font-semibold">18 km/h</p>
+                            <p className="text-lg font-semibold">Wind speed</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
